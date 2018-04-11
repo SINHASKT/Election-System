b802de89d5857735c8e04d8f30f7dcd35b44eabb
Sanket Sinha <sanketsinha1811@gmail.com>
2018-04-12 03:07:44 +0530

    Update send.php

--- send.php
+++ send.php
@@ -4,8 +4,8 @@ require_once 'autoload.php'; // Loads the library
 use Twilio\Rest\Client;
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "ACafad5d248afd2e8263adb66217a4d26e";
-$token = "5ab77eae305dcc05067ce28d2fde25d1";
+$sid = "";
+$token ="";
 $client = new Client($sid, $token);
 
 // Get an object from its sid. If you do not have a sid,
@@ -13,8 +13,8 @@ $client = new Client($sid, $token);
 $client->messages->create(
 '+918939308766',
 array(
-'from'=>'+1 309-271-0875 ',
-'body'=>'Motherfucker'
+'from'=>'',
+'body'=>
 )
 );
 ?>
